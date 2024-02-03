1b4331fe5bb66c26352a6042deda8c1a6480086d
BuickSalesman <adam.odonnell.e@gmail.com>
2024-02-03 11:25:12 -0500

    deliberate practice

--- 020324.js
+++ 020324.js
@@ -54,3 +54,26 @@ while (index1 < letters.length) {
   index1 += 1;
 }
 console.log(letterCombinations);
+
+// Use a nested loop to find the largest product of any two different numbers within a given array.
+// For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
+
+var numbers = [5, -2, 1, -9, -7, 2, 6];
+var maxProduct = numbers[0] * numbers[1];
+var index1 = 0;
+while (index1 < numbers.length) {
+  var currentNumber = numbers[index1];
+  index2 = 0;
+  while (index2 < numbers.length) {
+    if (index1 !== index2) {
+      var otherNumber = numbers[index2];
+      var product = currentNumber * otherNumber;
+      if (product > maxProduct) {
+        maxProduct = product;
+      }
+    }
+    index2 = index2 + 1;
+  }
+  index1 = index1 + 1;
+}
+console.log(maxProduct);
