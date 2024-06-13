c005a3e4759a58a0e6f65019458fa091ee02a95f
BuickSalesman <adam.odonnell.e@gmail.com>
2024-06-13 19:27:37 -0400

    deliberate practice

---
+++ 061324.js
@@ -0,0 +1,84 @@
+// Write a while loop to print the numbers 1 through 10.
+
+var index = 1;
+while (index <= 10) {
+  console.log(index);
+  index++;
+}
+
+// Write a while loop that prints the word "hello" 5 times.
+
+var index = 1;
+while (index <= 5) {
+  console.log("hello");
+  index++;
+}
+
+// Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+// while (true) {
+//   var input = window.prompt("Enter a word");
+//   if (input === "stop") {
+//     break;
+//   }
+// }
+
+// Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+var index = 0;
+while (index <= 100) {
+  console.log(index);
+  index = index + 5;
+}
+
+// Write a while loop that prints the number 9000 ten times.
+
+var powerLevel = 9000;
+var index = 0;
+while (index <= 4) {
+  console.log(powerLevel);
+  index++;
+}
+
+// Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// while (true) {
+//   var input = window.prompt("Enter a number: ");
+//   if (parseInt(input) > 10) {
+//     break;
+//   }
+// }
+
+// Write a while loop that prints the numbers 50 to 70.
+
+index = 50;
+while (index <= 70) {
+  console.log(index);
+  index++;
+}
+
+// Write a while loop that prints the phrase "Around the world" 144 times.
+
+var lyrics = "Around the world";
+var index = 0;
+while (index < 144) {
+  console.log(lyrics);
+  index++;
+}
+
+// Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+// while (true) {
+//   var input = window.prompt("Please enter a word: ");
+//   if (input.length > 5) {
+//     break;
+//   }
+// }
+
+// Write a while loop that prints the even numbers from 2 to 40.
+
+index = 2;
+while (index <= 40) {
+  console.log(index);
+  index = index + 2;
+}
