d8b00bcf966dfa79efed660dee594e89c8bbaf12
BuickSalesman <adam.odonnell.e@gmail.com>
2024-07-05 19:17:45 -0400

    deliberate practice

---
+++ 070524.js
@@ -0,0 +1,16 @@
+// Write a Song class with attributes and reader/writer methods for name, artist, and duration. Then write a method that prints the name, artist, and duration in a single sentence.
+
+class Song {
+  constructor(name, artist, duration) {
+    this.name = name;
+    this.artist = artist;
+    this.duration = duration;
+  }
+
+  printInfo() {
+    console.log(`The song ${this.name}, by ${this.artist}, has a duration of ${this.duration}`);
+  }
+}
+
+var song = new Song("Tomorrow Comes Today", "Gorillaz", "3:13");
+song.printInfo();
