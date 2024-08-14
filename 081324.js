c688995bc16cf3ae5235f8b03849a23999a1a49b
BuickSalesman <adam.odonnell.e@gmail.com>
2024-08-13 20:46:30 -0400

    deliberate practice

---
+++ 081324.js
@@ -0,0 +1,52 @@
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+
+var number = 10;
+console.log(number + 10);
+
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+var word = "hello";
+console.log(word.toUpperCase());
+
+// Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+var number1 = 3;
+var number2 = 13;
+console.log(number1 + number2);
+
+// Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+var word = "potbelly";
+console.log(word.split("").reverse().join(""));
+
+// Write a program that uses a variable to store a number, then prints the number times 10.
+
+var number = 8;
+console.log(number * 10);
+
+// Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+var word1 = "victoria";
+var word2 = "bacon";
+console.log(word1.toUpperCase() + word2.toUpperCase());
+
+// Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+var word = "revolve";
+console.log(word.length);
+
+// Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+var number = 7.984;
+console.log(parseInt(number));
+
+// Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+var number1 = 27;
+var number2 = 55;
+console.log(number1 * number2);
+
+// Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+var word = "sHYrOne";
+console.log(word.toLowerCase());
